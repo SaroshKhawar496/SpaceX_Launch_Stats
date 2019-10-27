09ebc8c5555836c9f245b04766bbcec431baac6c
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-26 22:10:29 -0400

    App created with react

---
+++ public/precache-manifest.d951cf04e6eeb72e7bc6d29b6145f5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b212d19606aee1b7789fddc945c0ac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd4a75b137ffc748c155",
+    "url": "/static/css/main.2a9f8799.chunk.css"
+  },
+  {
+    "revision": "996cc32983c7b74502d6",
+    "url": "/static/js/2.5fb1ce2d.chunk.js"
+  },
+  {
+    "revision": "dd4a75b137ffc748c155",
+    "url": "/static/js/main.d69adb20.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
+const path = require("path");
 
 const app = express();
+
+//Allow cross origin
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -12,6 +17,15 @@ app.use(
   })
 );
 
+// ---------------------------------------- for cloud deployment
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  //serving the react's index.html for all routes except for graphql server
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+// -----------------------------------------------
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
